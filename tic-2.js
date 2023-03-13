721498a607ed681b6d457c981fea1f63eab91fc3
Javier Ceballo <jvrceballo@gmail.com>
2023-03-13 14:26:48 -0400

    Add the code of the functions that write X or O according to the player's turn

---
+++ tic-2.js
@@ -0,0 +1,135 @@
+// Function called wherever user tab on any box
+function myfunc() {}
+
+// Function to reset game
+function reset() {}
+
+/* Functions to check turn of the player and
+put accordingly value X or O */
+flag = 1;
+function myfunc_1() {
+    if (flag == 1) {
+        document.getElementById("b1").value = "X";
+        document.getElementById("b1").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b1").value = "O";
+        document.getElementById("b1").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_2() {
+    if (flag == 1) {
+        document.getElementById("b2").value = "X";
+        document.getElementById("b2").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b2").value = "O";
+        document.getElementById("b2").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_3() {
+    if (flag == 1) {
+        document.getElementById("b3").value = "X";
+        document.getElementById("b3").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b3").value = "O";
+        document.getElementById("b3").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_4() {
+    if (flag == 1) {
+        document.getElementById("b4").value = "X";
+        document.getElementById("b4").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b4").value = "O";
+        document.getElementById("b4").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_5() {
+    if (flag == 1) {
+        document.getElementById("b5").value = "X";
+        document.getElementById("b5").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b5").value = "O";
+        document.getElementById("b5").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_6() {
+    if (flag == 1) {
+        document.getElementById("b6").value = "X";
+        document.getElementById("b6").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b6").value = "O";
+        document.getElementById("b6").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_7() {
+    if (flag == 1) {
+        document.getElementById("b7").value = "X";
+        document.getElementById("b7").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b7").value = "O";
+        document.getElementById("b7").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_8() {
+    if (flag == 1) {
+        document.getElementById("b8").value = "X";
+        document.getElementById("b8").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b8").value = "O";
+        document.getElementById("b8").disabled = true;
+        flag = 1;
+    }
+}
+
+function myfunc_9() {
+    if (flag == 1) {
+        document.getElementById("b9").value = "X";
+        document.getElementById("b9").disabled = true;
+        flag = 0;
+    }
+
+    else {
+        document.getElementById("b9").value = "O";
+        document.getElementById("b9").disabled = true;
+        flag = 1;
+    }
+}
+
