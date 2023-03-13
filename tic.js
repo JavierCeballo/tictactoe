bc2960e17e91c4c74bcbab1e807e0f42655ffa9a
Javier Ceballo <jvrceballo@gmail.com>
2023-03-13 14:03:29 -0400

    Add the game function declaration

---
+++ tic.js
@@ -0,0 +1,25 @@
+// Function called wherever user tab on any box
+function myfunc() {}
+
+// Function to reset game
+function reset() {}
+
+/* Functions to check turn of the player and
+put accordingly value X or O */
+function myfunc_1() {}
+
+function myfunc_2() {}
+
+function myfunc_3() {}
+
+function myfunc_4() {}
+
+function myfunc_5() {}
+
+function myfunc_6() {}
+
+function myfunc_7() {}
+
+function myfunc_8() {}
+
+function myfunc_9() {}
